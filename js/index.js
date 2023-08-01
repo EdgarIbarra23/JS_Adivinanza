0f2a30870345c18b4aa12d69cee135bc2ffc3b2b
EdgarIbarra23 <edgarfercho2309@gmail.com>
2023-08-01 13:35:29 -0500

    Second Project-JS

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function riddleNumber() {
     attempts += 1;
 
     if (add == '' ||add < 0 || add > 10 || isNaN(add)){
-        comments.textContent = 'Por favor Ingrese un Números Validos que este entre el 1 al 20';
+        comments.textContent = 'Por favor Ingrese un Números Validos que este entre el 1 al 10';
         return
     }
 
